55c89055af5ddbe6aa3a6b12c69400545043efbd
shoisob2004037 <mhshoisob@gmail.com>
2026-05-01 20:37:08 +0600

    update client

--- server/server.js
+++ server/server.js
@@ -20,8 +20,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://pharmacy-management-system-78if.vercel.app',
-    process.env.CLIENT_URL || 'http://localhost:3000'
+    'https://online-bus-ticket-booking-ai-chatbo-wheat.vercel.app',
+    process.env.CLIENT_URL
   ],
   credentials: true
 }));
